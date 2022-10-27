d48aee4ad70f8d61bafa1469aaed0bb9d972189a
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2022-10-27 12:33:01 +0200

    v4.9

--- Happy Travel/access/js/main.js
+++ Happy Travel/access/js/main.js
@@ -1,15 +1,25 @@
 function Barcelona() {
-    document.getElementById("mini_content_ss").innerHTML = "Barcelona ligt in Spanje en is een zonnige stad met veel inwoners. Geniet van het mooie weer, of cruse lekker met een boot door het water! Er valt ook genoeg te shoppen en te ontdekken! Dit is Barcelona. (Gidswandeling inbegrepen)";
+    var1 = "Barcelona ligt in Spanje en is een zonnige stad met veel inwoners. Geniet van het mooie weer, of cruse lekker met een boot door het water! Er valt ook genoeg te shoppen en te ontdekken! Dit is Barcelona. (Gidswandeling inbegrepen)";
+    document.getElementById("mini_content_ss").innerHTML = var1;
+    document.getElementById("mini_content_ss_mobile").innerHTML = var1;
 }
 function Maastricht() {
-    document.getElementById("mini_content_ss").innerHTML = "Maastricht ligt in Nederland en is een vrolijke en gezellige stad. Je kan er veel winkels bezoeken en er zijn vele activiteiten in de buurt, zoals de 'Grotten' van Valkenburg. Verder zijn er vele wandelroutes te belopen. Dit is Maastricht (Exclusief Gidswandeling)";
+    var1 = "Maastricht ligt in Nederland en is een vrolijke en gezellige stad. Je kan er veel winkels bezoeken en er zijn vele activiteiten in de buurt, zoals de 'Grotten' van Valkenburg. Verder zijn er vele wandelroutes te belopen. Dit is Maastricht (Exclusief Gidswandeling)";
+    document.getElementById("mini_content_ss").innerHTML = var1;
+    document.getElementById("mini_content_ss_mobile").innerHTML = var1;
 }
 function Londen() {
-    document.getElementById("mini_content_ss").innerHTML = "Londen ligt in 'The UK' en is een drukke en bekende stad. Londen heeft veel geschiedenis en er valt daar van alles te beleven. Zo heb je 'The London Eye.' Dat is een reuzenrad wat een van de grootsten van europa is. Je kan overal wel in een kroeg komen in Londen. Dit is Londen. (Gidswandeling inbegrepen)";
+    var1 = "Londen ligt in 'The UK' en is een drukke en bekende stad. Londen heeft veel geschiedenis en er valt daar van alles te beleven. Zo heb je 'The London Eye.' Dat is een reuzenrad wat een van de grootsten van europa is. Je kan overal wel in een kroeg komen in Londen. Dit is Londen. (Gidswandeling inbegrepen)";
+    document.getElementById("mini_content_ss").innerHTML = var1;
+    document.getElementById("mini_content_ss_mobile").innerHTML = var1;
 }
 function Curacao() {
-    document.getElementById("mini_content_ss").innerHTML = "Curacao is een zonnig eiland geschikt voor tourisme. Er zijn er veel mooie hotels en bouwwerken te vinden. Je kan er zwemmen. Dit is Curacao. (Exclusief gidswandeling)";
+    var1 = "Curacao is een zonnig eiland geschikt voor tourisme. Er zijn er veel mooie hotels en bouwwerken te vinden. Je kan er zwemmen. Dit is Curacao. (Exclusief gidswandeling)";
+    document.getElementById("mini_content_ss").innerHTML = var1;
+    document.getElementById("mini_content_ss_mobile").innerHTML = var1;
 }
 function NY() {
-    document.getElementById("mini_content_ss").innerHTML = "NewYork is een stad in Amerika. De stad heeft veel geschiedenis en is hypermodern. Zo heb je er veel kantoren en veel flat's. NewYork is echt een drukke en grote stad waar ook veel restaurantz zijn en waar ook overal wel wat te beleven valt. Dit is NewYork. (Gidswandeling inbegrepen)";
+    var1 = "NewYork is een stad in Amerika. De stad heeft veel geschiedenis en is hypermodern. Zo heb je er veel kantoren en veel flat's. NewYork is echt een drukke en grote stad waar ook veel restaurantz zijn en waar ook overal wel wat te beleven valt. Dit is NewYork. (Gidswandeling inbegrepen)";
+    document.getElementById("mini_content_ss").innerHTML = var1;
+    document.getElementById("mini_content_ss_mobile").innerHTML = var1;
 }
