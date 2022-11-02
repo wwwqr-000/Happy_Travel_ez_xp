c9511649e9052d460553c9acf6bf34323c925061
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2022-11-02 22:59:29 +0100

    v6.2

--- Happy Travel/access/js/main.js
+++ Happy Travel/access/js/main.js
@@ -1,3 +1,4 @@
+//Stedentrips
 function Barcelona() {
     var1 = "Barcelona ligt in Spanje en is een zonnige stad met veel inwoners. Geniet van het mooie weer, of cruse lekker met een boot door het water! Er valt ook genoeg te shoppen en te ontdekken! Dit is Barcelona. (Gidswandeling inbegrepen)";
     document.getElementById("mini_content_ss").innerHTML = var1;
@@ -47,6 +48,7 @@ function verzending() {
         invalid_character();
     }
 }
+//Last Minute
 function Colombia() {
     var1 = "(Deze reis is beschikbaar tot morgen om 12:30) 'Colombia is een ideale stad om drugs te dealen!' ~ (Ft. Pablo) Verder is Colombia een mooi en zonnige stad om je vakantie te vieren! Er zijn veel verschillende culturen in Colombia en daardoor valt er veel te beleven. Al vanaf &euro;321. Ga naar tickets om uw last minute ticket te halen! Dit is Colombia!";
     document.getElementById("main_txt_lm").innerHTML = var1;
@@ -60,7 +62,7 @@ function Parijs() {
     document.getElementById("main_txt_lm").innerHTML = var1;
 }
 function Alpen() {
-    var1 = "(Deze reis is beschikbaar tot woensdag om 9:00) De Alpen is een prachtig berggebied in Europa! Je kan er allerlei winteractiviteiten doen, op een terrasje gaan zitten, een Westereenequipment kopen, enz. Er valt genoeg te beleven. Ga naar tickets om uw last minute tickets te halen! Dit zijn de Alpen!";
+    var1 = "(Deze reis is beschikbaar tot woensdag om 9:00) De Alpen is een prachtig berggebied in Europa! Je kan er allerlei winteractiviteiten doen, op een terrasje gaan zitten, een Westereenequipment kopen, enz. Er valt genoeg te beleven. Al vanaf &euro;280. Ga naar tickets om uw last minute tickets te halen! Dit zijn de Alpen!";
     document.getElementById("main_txt_lm").innerHTML = var1;
 }
 function Lapalma() {
@@ -68,6 +70,11 @@ function Lapalma() {
     document.getElementById("main_txt_lm").innerHTML = var1;
 }
 function Cyprus() {
-    var1 = "(Deze reis is beschikbaar tot maandag om 17:28) Cyprus is een oost-middelandsezee gelegen eiland. Het land ligt tussen Turkije en Syrie in. Het land heeft twee politieken en je kan er mooi buiten zitten. Ook heb je er veel water activiteiten. Ga naar tickets om uw last minute tickets te halen! Dit is Cyprus!";
+    var1 = "(Deze reis is beschikbaar tot maandag om 17:28) Cyprus is een oost-middelandsezee gelegen eiland. Het land ligt tussen Turkije en Syrie in. Het land heeft twee politieken en je kan er mooi buiten zitten. Ook heb je er veel water activiteiten. Al vanaf &euro;305 Ga naar tickets om uw last minute tickets te halen! Dit is Cyprus!";
     document.getElementById("main_txt_lm").innerHTML = var1;
+}
+//Verre Reizen
+function India() {
+    var1 = "India is een groot land met grote en vele culturen. Het land heeft ook veel geloven. Je kan er ook komen voor de natuur die er ontdanks de drukke steden ook is. Al vanaf &euro;490 kunt u voor een midweek in dit gebouw doorbrengen. Ga naar tickets om uw reis te boeken! Dit is India!";
+    document.getElementById("mini_content_ss").innerHTML = var1;
 }
